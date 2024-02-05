57d7f5be5cd11886d4c7c351bd50eb26cf994053
miraiy-outh <shokolatemeur@mail.ru>
2024-02-05 16:20:11 +0300

    add pagination

--- src/pages/products-list/products-list.tsx
+++ src/pages/products-list/products-list.tsx
@@ -3,15 +3,21 @@ import { Footer } from "../../components/footer/footer";
 import { Header } from "../../components/header/header";
 import { Products } from "./products/products";
 import { FilterList } from "./filter-list/filter-list";
+import { Pagination } from "./pagination/pagination";
 
 export function ProductsList() {
     return (
         <div className="products-list">
             <Header />
             <div className="products-list__container">
-                <Products />
+                <div className="products-list__container__subcontainer">
+                    <Products />
+                    <Pagination />
+
+                </div>
                 <FilterList />
             </div>
+
             <Footer />
         </div>
     )
